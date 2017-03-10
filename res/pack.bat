a87f55734c4b534cef5f8fc68e1f181e50714596
fghcvjk <243052686@qq.com>
2017-03-10 14:34:22 +0800

    统一用pyside处理

--- res/pack.bat
+++ res/pack.bat
@@ -1,4 +1,4 @@
 pyside-uic mainUI.ui -o ../mainUI.py
 pyside-uic addGameUI.ui -o ../addGameUI.py
 pyside-uic rmGameUI.ui -o ../rmGameUI.py
-pyrcc4 ico.qrc -o ../ico_rc.py
+pyside-rcc ico.qrc -o ../ico_rc.py
